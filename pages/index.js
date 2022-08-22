42a4e72494635fc8eab4fd9b57cb51ba683ef422
ralexs1991 <smithmra1991@gmail.com>
2022-08-22 18:42:36 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ function Home() {
     const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
     console.log(`URL: ${API_URL}`)
     const [query, setQuery] = useState("");
-    const link = new HttpLink({ uri: `${NEXT_PUBLIC_API_URL}/graphql`})
+    const link = new HttpLink({ uri: `${API_URL}/graphql`})
     const cache = new InMemoryCache()
     const client = new ApolloClient({link,cache});
     const appContext = useContext(AppContext);
