0a6ce9026980a9670d0064268292f1681c5565ef
ralexs1991 <smithmra1991@gmail.com>
2022-08-22 18:24:46 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import AppContext from './../components/context';
 
 function Home() {
     const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
-    console.log(`URL: ${NEXT_PUBLIC_API_URL}`)
+    console.log(`URL: ${API_URL}`)
     const [query, setQuery] = useState("");
     const link = new HttpLink({ uri: `${NEXT_PUBLIC_API_URL/graphql`})
     const cache = new InMemoryCache()
